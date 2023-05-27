fff18f310ff228f39d3dc261c2c1caae6db78dd7
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2023-05-27 09:42:40 -0600

    scroll and value

--- $.js
+++ $.js
@@ -22,11 +22,11 @@
  * 	[key:number]: Query
  * 	[Symbol.iterator]: () => IterableIterator<Query>;
  * }} QueryList
-*/
+ */
 
 /**
  * @typedef {HTMLElement | Node | ParentNode} QueryNode
-*/
+ */
 
 /**
  * @param {string | NodeList | QueryNode | null} node
@@ -68,13 +68,64 @@ export function $(node) {
  */
 $.ƒ = function (element) {
 	let query = null;
-	let _ =
-		fn =>
-		(...args) =>
-			fn(...args) || query;
+	let _ = (fn, props) => {
+		const FN = (...args) => fn(...args) || query;
+		Object.assign(FN, props);
+		return FN;
+	};
 	query = new Proxy(element, {
 		get(target, property) {
 			if (property == 'forEach') return;
+			if (property == 'scroll')
+				return {
+					to: _((options, y) => {
+						if (typeof options === 'number' && typeof y === 'number') target.scrollTo(options, y);
+						else if (typeof options === 'object') target.scrollTo(options);
+						return query['scroll'];
+					}),
+					top: _((position, behavior) => {
+						target.scrollTo({ top: position, behavior });
+						return query['scroll'];
+					}),
+					left: _((position, behavior) => {
+						target.scrollTo({ left: position, behavior });
+						return query['scroll'];
+					}),
+					height: _(() => {
+						return target.scrollHeight;
+					}),
+					width: _(() => {
+						return target.scrollWidth;
+					}),
+					bottom: _((position, behavior) => {
+						target.scrollTo({ top: target.scrollHeight - position, behavior });
+						return query['scroll'];
+					}),
+					right: _((position, behavior) => {
+						target.scrollTo({ left: target.scrollWidth - position, behavior });
+						return query['scroll'];
+					}),
+				};
+			if (property == 'value')
+				return _(
+					value => {
+						// @ts-ignore
+						if (!target?.value) return;
+						// @ts-ignore
+						if (typeof value === 'string') target.value = value;
+						// @ts-ignore
+						else return target.value;
+					},
+					{
+						push(value) {
+							// @ts-ignore
+							if (!target?.value) return;
+							// @ts-ignore
+							target.value += value;
+							return query['value'];
+						},
+					}
+				);
 			if (property == 'element') {
 				return target;
 			}
@@ -110,49 +161,49 @@ $.ƒ = function (element) {
 					return target;
 				});
 			}
-			if(property == 'prepend'){
+			if (property == 'prepend') {
 				return _(data => {
 					target.prepend(data);
 					return target;
-				})
+				});
 			}
-			if(property == 'childrens'){
-				return _( data => {
-					return $(target.childNodes)
-				})
-			}
-			if(property == 'parent'){
+			if (property == 'childrens') {
 				return _(data => {
-					return $(target.parentNode)
-				})
+					return $(target.childNodes);
+				});
+			}
+			if (property == 'parent') {
+				return _(data => {
+					return $(target.parentNode);
+				});
 			}
 			if (property == 'getAttribute') {
 				return _(name => {
 					return target.getAttribute(name);
 				});
 			}
-			if(property == 'removeAttribute'){
+			if (property == 'removeAttribute') {
 				return _(name => {
 					target.removeAttribute(name);
 					return target;
-				})
+				});
 			}
-			if(property == 'setAttribute'){
+			if (property == 'setAttribute') {
 				return _((name, value) => {
 					target.setAttribute(name, value);
 					return target;
-				})
+				});
 			}
-			if(property == 'remove'){
+			if (property == 'remove') {
 				return _(() => {
 					target.remove();
 					return target;
-				})
+				});
 			}
-			if(property == 'clone'){
+			if (property == 'clone') {
 				return _(v => {
 					return $(target.cloneNode(v));
-				})
+				});
 			}
 			return _(callback => {
 				query.on(property, (...args) => {
