e14f25b0b00787381124783f61ed96888465cb16
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2023-05-28 22:29:05 -0600

    target

--- SPA.js
+++ SPA.js
@@ -9,12 +9,12 @@ export const makeLink = (callback) => function Link({target, to, ...props}){
   const handleClick = (e) => {
     const isMainEvent = e.button === 0; // primary click
     const isModifierEvent = e.metaKey || e.altKey || e.ctrlKey || e.shiftKey;
-    const isManegableEvent = !target || target === '_self';
+    const isManegableEvent = !props.target || props.target === '_self';
 
     if (isMainEvent && isManegableEvent && !isModifierEvent) {
       e.preventDefault();
       navigateTo(to, callback);
     } 
   }
-  return dom('a', {href: to, ...props, onClick: handleClick, target})
+  return dom('a', {href: to, ...props, onClick: handleClick})
 }
