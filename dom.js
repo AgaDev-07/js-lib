94e11d88f1981ca131404c48ce668f8bec3b2125
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2023-05-28 22:23:27 -0600

    childrens to props

--- dom.js
+++ dom.js
@@ -2,94 +2,91 @@
  * @param {string} type
  * @param {Object} props
  * @param {(HTMLHeadingElement | string | number)[]} args
-*/
+ */
 export function dom(type, props, ...args) {
-  const childrens = args.length ? [].concat(...args) : null;
-  return {
-      type,
-      props: props || {},
-      childrens
-  };
+	const childrens = args.length ? [].concat(...args) : null;
+	return {
+		type,
+		props: props || {},
+		childrens,
+	};
 }
-//#region 
+//#region
 function isEventProp(name) {
-  return /^on/.test(name);
+	return /^on/.test(name);
 }
 function extractEventName(name) {
-  return name.slice(2).toLowerCase();
+	return name.slice(2).toLowerCase();
 }
 function setProp(node, name, value) {
-  if (name === "className") {
-      name = "class";
-  }
-  if (typeof value === "boolean") {
-      value = setBooleanProp(node, name, value);
-  } else if (value instanceof Array) {
-      value = value.join(' ')
-  }
-  node.setAttribute(name, value);
+	if (name === 'className') {
+		name = 'class';
+	}
+	if (typeof value === 'boolean') {
+		value = setBooleanProp(node, name, value);
+	} else if (value instanceof Array) {
+		value = value.join(' ');
+	}
+	node.setAttribute(name, value);
 }
 function setBooleanProp(node, name, value) {
-  if (value) {
-      node[name] = true;
-  } else {
-      node[name] = false;
-  }
-  return node[name]
+	if (value) {
+		node[name] = true;
+	} else {
+		node[name] = false;
+	}
+	return node[name];
 }
 function setAttributes(node, props = {}) {
-  if (!props) {
-      return;
-  }
-  Object.keys(props)
-      .filter(prop => !isEventProp(prop))
-      .forEach(name => setProp(node, name, props[name]));
+	if (!props) {
+		return;
+	}
+	Object.keys(props)
+		.filter(prop => !isEventProp(prop))
+		.forEach(name => setProp(node, name, props[name]));
 }
 function addEventListeners(node, props = {}) {
-  if (!props) {
-      return;
-  }
-  Object.keys(props)
-      .filter(isEventProp)
-      .forEach(event =>
-          node.addEventListener(extractEventName(event), props[event])
-      );
+	if (!props) {
+		return;
+	}
+	Object.keys(props)
+		.filter(isEventProp)
+		.forEach(event => node.addEventListener(extractEventName(event), props[event]));
 }
 //#endregion
 
 /**
-* @param {string | number | object} node
-* @return {HTMLElement}
-*/
+ * @param {string | number | object} node
+ * @return {HTMLElement}
+ */
 export function createElement(node) {
-  if (typeof node === "string" || typeof node === "number") {
-      return document.createTextNode(node);
-  }
-  if (node instanceof HTMLElement) return node
+	if (typeof node === 'string' || typeof node === 'number') {
+		return document.createTextNode(node);
+	}
+	if (node instanceof HTMLElement) return node;
 
-  if (typeof node.type === "function") {
-      return createElement(node.type(node.props));
-  }
+	if (node.childrens && node.childrens.length > 0) {
+		node.props.childrens = node.props.childrens || [];
+		node.props.childrens.push(...node.childrens.map(createElement));
+	}
 
-  const element = document.createElement(node.type);
+	if (typeof node.type === 'function') {
+		return createElement(node.type(node.props));
+	}
 
-  setAttributes(element, node.props);
+	const element = document.createElement(node.type);
 
-  addEventListeners(element, node.props);
+	setAttributes(element, node.props);
 
-  node.childrens &&
-      node.childrens
-          .map(createElement)
-          .forEach(child => element.appendChild(child));
-
-  return element;
+	addEventListeners(element, node.props);
+	return element;
 }
 /**
-* @param {string} type
-* @param {Object} props
-* @param {(HTMLElement | string | number)[]} args
-*/
+ * @param {string} type
+ * @param {Object} props
+ * @param {(HTMLElement | string | number)[]} args
+ */
 export function createElementDom(type, props, ...args) {
-  return createElement(dom(type, props, ...args))
+	return createElement(dom(type, props, ...args));
 }
-createElement.dom = createElementDom
+createElement.dom = createElementDom;
