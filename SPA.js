4d811eacbb8cd94ec507e6e2429cc27cc22945c7
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2023-05-28 21:16:30 -0600

    Link return nodeDom

---
+++ SPA.js
@@ -0,0 +1,20 @@
+
+import {dom} from './dom.js'
+export function navigateTo(path, callback) {
+  history.pushState({}, null, path);
+  window.dispatchEvent(new Event('pushstate'));
+  callback && callback();
+}
+export const makeLink = (callback) => function Link({target, to, ...props}){
+  const handleClick = (e) => {
+    const isMainEvent = e.button === 0; // primary click
+    const isModifierEvent = e.metaKey || e.altKey || e.ctrlKey || e.shiftKey;
+    const isManegableEvent = !target || target === '_self';
+
+    if (isMainEvent && isManegableEvent && !isModifierEvent) {
+      e.preventDefault();
+      navigateTo(to, callback);
+    } 
+  }
+  return dom('a', {href: to, ...props, onClick: handleClick, target})
+}
